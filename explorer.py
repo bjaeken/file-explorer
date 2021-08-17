a77f057c6546b4a715444818abf76e3f9c925ba1
bjaeken <33750290+bjaeken@users.noreply.github.com>
2021-08-17 18:12:27 +0200

    Added functionality to fetch the content from given path

--- explorer.py
+++ explorer.py
@@ -1,17 +1,34 @@
+from item import Item
+from pathlib import Path
+
 class Explorer:
     def __init__(self):
-        self.path = "/"
+        self.path = "/home/pi"
+        self.lib = Path(self.path)
         self.content = list()
+        self.fetch_content()
 
     def get_path(self):
         return self.path
     
     def get_content(self):
-        content = ""
-        for item in range(10):
-            content += str(item) + "\n"
+        items = ""
+        for item in self.content:
+            items += item.__str__()
 
-        return content
+        return items
     
-    def fetch_content(path):
-        print("fetching")
+    def fetch_content(self):
+        self.content.clear()
+
+        if self.lib.is_dir():
+            for item in self.lib.glob("*"):
+                if self.check_hidden_item(item):
+                    if item.is_dir():
+                        self.content.append(Item(item.name, True, item.stat().st_size))
+                    else:
+                        self.content.append(Item(item.name, False, item.stat().st_size))
+
+    def check_hidden_item(self, item):
+        return (not item.name.startswith("."))
+           
